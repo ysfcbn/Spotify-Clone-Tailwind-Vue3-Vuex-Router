5a3e22765267a88bcb1373c783b9f79a14e6dba2
ysfcbn <pyusuf24@gmail.com>
2023-07-18 19:18:48 +0300

    update searchItem

--- src/components/Search/SearchResult.vue
+++ src/components/Search/SearchResult.vue
@@ -1,96 +1,98 @@
 <template>
-	<div class="Search-Result p-4 lg:ml-[1rem] mx-4">
-		<div class="relative w-full h-full grid col-span-full pb-5">
-			<div class="flex items-center justify-start text-sm">
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 transition duration-200"
-				>
-					<span class="text-white">All</span>
-				</button>
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] mx-2 cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200"
-				>
-					<span class="text-white">Playlists</span>
-				</button>
-				<button
-					class="min-w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200"
-				>
-					<span class="text-white">Podcasts & Shows</span>
-				</button>
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
-				>
-					<span class="text-white">Artists</span>
-				</button>
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
-				>
-					<span class="text-white">Songs</span>
-				</button>
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
-				>
-					<span class="text-white">Profiles</span>
-				</button>
-				<button
-					class="min-w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
-				>
-					<span class="text-white">Genres & Moods</span>
-				</button>
-				<button
-					class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
-				>
-					<span class="text-white">Albums</span>
-				</button>
-			</div>
-		</div>
-	</div>
+  <div class="Search-Result p-4 lg:ml-[1rem] mx-4">
+    <div class="relative w-full h-full grid col-span-full pb-5">
+      <div class="flex items-center justify-start text-sm overflow-x-scrool">
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 transition duration-200"
+        >
+          <span class="text-white">All</span>
+        </button>
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] mx-2 cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200"
+        >
+          <span class="text-white">Playlists</span>
+        </button>
+        <button
+          class="min-w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200"
+        >
+          <span class="text-white">Podcasts & Shows</span>
+        </button>
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
+        >
+          <span class="text-white">Artists</span>
+        </button>
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
+        >
+          <span class="text-white">Songs</span>
+        </button>
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
+        >
+          <span class="text-white">Profiles</span>
+        </button>
+        <button
+          class="min-w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
+        >
+          <span class="text-white">Genres & Moods</span>
+        </button>
+        <button
+          class="w-fit h-fit rounded-full px-3 py-[6px] cursor-default bg-dark2 hover:bg-dark3/70 transition duration-200 mx-2"
+        >
+          <span class="text-white">Albums</span>
+        </button>
+      </div>
+    </div>
+    <div class="relative w-[5rem] h-[5rem] bg-dark2"></div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-	data() {
-		return {};
-	},
-	methods: {
-		async fetchSearchItem() {
-			await axios
-				.get(
-					`https://api.spotify.com/v1/search?q=${this.searchItem}&type=${this.types}`,
-					{
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json',
-							Authorization: 'Bearer ' + this.getToken,
-						},
-					}
-				)
-				.then(({ data }) => {
-					console.log(data);
-				})
-				.catch(err => console.log(err));
-		},
-	},
-	computed: {
-		isAuth() {
-			return this.$store.getters.isAuth;
-		},
-		getToken() {
-			return this.$store.getters.accessToken;
-		},
-		searchItem() {
-			return this.$store.getters['searchItem/getSearchItem'];
-		},
-		types() {
-			return this.$store.getters['searchItem/getTypes'];
-		},
-	},
-	mounted() {
-		console.log('searchResult Mounted!');
-		this.fetchSearchItem();
-	},
+  data() {
+    return {};
+  },
+  methods: {
+    async fetchSearchItem() {
+      await axios
+        .get(
+          `https://api.spotify.com/v1/search?q=${this.searchItem}&type=${this.types}`,
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + this.getToken,
+            },
+          }
+        )
+        .then(({ data }) => {
+          console.log(data);
+          this.$store.dispatch("searchItem/searchResultArr", data);
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.isAuth;
+    },
+    getToken() {
+      return this.$store.getters.accessToken;
+    },
+    searchItem() {
+      return this.$store.getters["searchItem/getSearchItem"];
+    },
+    types() {
+      return this.$store.getters["searchItem/getTypes"];
+    },
+  },
+  mounted() {
+    console.log("searchResult Mounted!");
+    this.fetchSearchItem();
+  },
 };
 </script>
 
--- src/store/modules/searchItem.js
+++ src/store/modules/searchItem.js
@@ -1,27 +1,37 @@
 const searchItemModule = {
-	namespaced: true,
-	state: {
-		searchItem: '',
-		types: ['album', 'artist', 'playlist', 'track', 'show', 'episode'],
-	},
-	mutations: {
-		searchItem(state, payload) {
-			state.searchItem = payload;
-		},
-	},
-	actions: {
-		async searchItem({ commit }, payload) {
-			commit('searchItem', await payload);
-		},
-	},
-	getters: {
-		getSearchItem(state) {
-			return state.searchItem;
-		},
-		getTypes(state) {
-			return state.types;
-		},
-	},
+  namespaced: true,
+  state: {
+    searchItem: "",
+    types: ["album", "artist", "playlist", "track", "show", "episode"],
+    searchResultArr: null,
+  },
+  mutations: {
+    searchItem(state, payload) {
+      state.searchItem = payload;
+    },
+    searchResultArr(state, payload) {
+      state.searchResultArr = payload;
+    },
+  },
+  actions: {
+    async searchItem({ commit }, payload) {
+      commit("searchItem", await payload);
+    },
+    async searchResultArr({ commit }, payload) {
+      commit("searchResultArr", await payload);
+    },
+  },
+  getters: {
+    getSearchItem(state) {
+      return state.searchItem;
+    },
+    getTypes(state) {
+      return state.types;
+    },
+    getSearchResultArr(state) {
+      return state.searchResultArr;
+    },
+  },
 };
 
 export default searchItemModule;
