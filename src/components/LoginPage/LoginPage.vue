bc49b9089831227d1ee6159c94cab13c724db43b
Yusuf ÇOBAN <pyusuf24@gmail.com>
2023-08-02 13:05:30 +0300

    rid off client id

--- src/components/LoginPage/LoginPage.vue
+++ src/components/LoginPage/LoginPage.vue
@@ -133,7 +133,6 @@
 						>{{ client_Secret.length }}/32</span
 					>
 				</div>
-				<p class="text-black">975fd3eb76424ac49bd2a701a015fc89</p>
 
 				<div class="w-full flex justify-content mt-2">
 					<button
