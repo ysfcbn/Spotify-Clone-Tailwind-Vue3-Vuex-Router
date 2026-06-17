b311edc7186c0b7ed4aaaea4f80965f922ae5a8d
Yusuf ÇOBAN <pyusuf24@gmail.com>
2026-06-18 00:13:24 +0300

    Add files via upload

--- src/components/LoginPage/auth.js
+++ src/components/LoginPage/auth.js
@@ -1,49 +1,79 @@
-export const getAuth = client_id => {
-	const REDİRECT_URL = encodeURIComponent(
-		`${window.location.origin}/login`
-	);
-	console.log(REDİRECT_URL);
-	const CLIENT_ID = encodeURIComponent(client_id);
+/*
+export const getAuth = (client_id) => {
+  const REDİRECT_URL = encodeURIComponent(
+    `https://spotify-clone-vue3.vercel.app/login`
+  );
+  console.log(REDİRECT_URL);
+  const CLIENT_ID = encodeURIComponent(client_id);
 
-	const scopes =
-		'ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email';
-	const SCOPE_URL_PARAM = encodeURIComponent(scopes);
-	const URL = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${SCOPE_URL_PARAM}&redirect_uri=${REDİRECT_URL}&show_dialog=false`;
+  const scopes =
+    "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email";
+  const SCOPE_URL_PARAM = encodeURIComponent(scopes);
+  const URL = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${SCOPE_URL_PARAM}&redirect_uri=${REDİRECT_URL}&show_dialog=false`;
 
-	(function () {
-		window.location = URL;
-	})();
+  (function () {
+    window.location = URL;
+  })();
 };
 
-export const accessToken = hash => {
-	const hashParams = hash.substring(1).split('&');
-	const reducedHashParams = hashParams.reduce((acc, currentVal) => {
-		const [key, value] = currentVal.split('=');
-		acc[key] = value;
-		return acc;
-	}, {});
-	return reducedHashParams.access_token;
+export const accessToken = (hash) => {
+  const hashParams = hash.substring(1).split("&");
+  const reducedHashParams = hashParams.reduce((acc, currentVal) => {
+    const [key, value] = currentVal.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  return reducedHashParams.access_token;
 };
 
-// 'https://spotify-clone-music.netlify.app/login'
+*/
 
-// Client_Credential Auth flow
-/* export const getAuth = async (encodedID) => {
+const REDIRECT_URI = "https://spotify-clone-vue3.vercel.app/login";
 
- const authParameters = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + encodedID,
-    },
-    body: 'grant_type=client_credentials',
-  };
-  return await fetch('https://accounts.spotify.com/api/token', authParameters)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      return data.access_token;
-    })
-    .catch((err) => console.log(err));
-	*/
+const generateRandomString = (length) => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let text = "";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
+
+const generateCodeChallenge = async (verifier) => {
+  const data = new TextEncoder().encode(verifier);
+
+  const digest = await crypto.subtle.digest("SHA-256", data);
+
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+};
+
+export const getAuth = async (clientId) => {
+  const scopes =
+    "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email";
+
+  const codeVerifier = generateRandomString(128);
+
+  localStorage.setItem("code_verifier", codeVerifier);
+
+  const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+  const authUrl = new URL("https://accounts.spotify.com/authorize");
+
+  authUrl.search = new URLSearchParams({
+    client_id: clientId,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    scope: scopes,
+    code_challenge_method: "S256",
+    code_challenge: codeChallenge,
+  }).toString();
+
+  window.location.href = authUrl.toString();
+};
